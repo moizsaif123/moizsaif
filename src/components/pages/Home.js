4fc1febeef904dcfb9aa8d4db215ea7d795d00b0
MoizSaifuddin <moizsaifuddin123@gmail.com>
2024-03-03 18:51:02 -0500

    updated home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
+import CardItem from '../CardItem';
 
 function Home(){
     return (
         <>
             <HeroSection />
             <Cards />
-
+            <CardItem />
         </>
     );
 }
